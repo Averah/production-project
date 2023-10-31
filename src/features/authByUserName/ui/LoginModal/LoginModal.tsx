12ccad4f72fde1cf336cd8c4cc01b89686c6d372
Yuliya K <yuli-kretova@yndex.ru>
2023-10-31 17:10:44 +0300

    fix tslint

--- src/features/authByUserName/ui/LoginModal/LoginModal.tsx
+++ src/features/authByUserName/ui/LoginModal/LoginModal.tsx
@@ -1,7 +1,5 @@
 import React, { FC } from 'react';
-import classNames from 'classnames';
 import { Modal } from 'shared/ui/Modal/Modal';
-import cls from './LoginModal.module.scss';
 import { LoginForm } from '../LoginForm/LoginForm';
 
 interface LoginModalProps {
@@ -14,7 +12,7 @@ export const LoginModal: FC<LoginModalProps> = (props) => {
     const { className, isOpen, onClose } = props;
 
     return (
-        <div className={classNames(cls.LoginModal, className)}>
+        <div className={className}>
             <Modal isOpen={isOpen} onClose={onClose}>
                 <LoginForm />
             </Modal>
